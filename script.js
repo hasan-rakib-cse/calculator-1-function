de31b8dd80f95023494aaf140cbf5255a470ad2c
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-09-13 16:09:43 +0600

    all done

---
+++ script.js
@@ -0,0 +1,32 @@
+let input = document.getElementById('inputBox');
+let buttons = document.querySelectorAll('button');
+
+let string = ""; // for storing result
+let arr = Array.from(buttons); //buttton gulo nea 1ta array banea debe.
+
+arr.forEach(button => {
+    button.addEventListener('click', (e) => {
+        if(e.target.innerHTML == '=') {
+            if((string).includes('.')) {
+                string = eval(string).toFixed(2);
+                input.value = string;
+            }
+            else {
+                string = eval(string);
+                input.value = string;
+            }
+        }
+        else if(e.target.innerHTML == 'AC') {
+            string = ''
+            input.value = string;
+        }
+        else if(e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            input.value = string;
+        }
+        else {
+            string += e.target.innerHTML;
+            input.value = string;
+        }
+    })
+});
